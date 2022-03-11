ee01a72be71441123c7a05dc8113d9e7f3a1676b
Vanshaj Poonia <Vanshajtheunique@gmail.com>
2022-03-11 21:15:08 +0530

    Initial Commit

---
+++ static/dummyEvents.js
@@ -0,0 +1,14 @@
+export const dummyEvents = [
+  {
+    price: 0.02,
+    from: '0xBb8888e699965C3de7b398187E5e3535b08F7232',
+    to: '0x9fE40fd755EF47537aF8AE12711dC7FE48d28D9B',
+    date: '3 weeks ago',
+  },
+  {
+    price: 0.1,
+    from: '0xBb8888e699965C3de7b398187E5e3535b08F7232',
+    to: '0xBb8888e699965C3de7b398187E5e3535b08F7232',
+    date: '4 months ago',
+  },
+]
